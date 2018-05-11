7f3738fc296484e737fb286c62e0af7234220dda
MAUS502 <manngt@gmail.com>
2018-05-10 22:26:09 -0600

    Obtain measures

--- app/Http/Controllers/WaterMeasureController.php
+++ app/Http/Controllers/WaterMeasureController.php
@@ -9,6 +9,14 @@ use Symfony\Component\Debug\Exception\FatalThrowableError;
 
 class WaterMeasureController extends Controller
 {
+    public function index()
+    {
+        $measures = WaterMeasure::all();
+
+        return[
+            'measures' => $measures
+        ];
+    }
     public function store(Request $request)
     {
         $status = 404;
